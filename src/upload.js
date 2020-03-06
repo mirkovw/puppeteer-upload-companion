45d50e2fe3a32d962d5ed1428c8aa9cc0570312b
Mirko van Willigenburg <mirko.van.willigenburg@gmail.com>
2020-03-06 17:29:45 +0100

    working upload script

--- src/upload.js
+++ src/upload.js
@@ -1,5 +1,6 @@
-const config = require('config');
 const puppeteer = require('puppeteer');
+const config = require('config');
+const path = require('path');
 
 const mainPageUrl = 'https://www.google.com/doubleclick/studio/'
 const advertisersPageUrl = mainPageUrl + '#advertisers:';
@@ -10,7 +11,7 @@ const newCampaignPageUrl = mainPageUrl + '#campaign/new:';
 const newCreativePageUrl = mainPageUrl + '#creative/new:';
 
 const accountName = 'Mediamonks';
-const advertiserName = 'dcu_test_advertiser';
+const advertiserName = 'netflix';
 const campaignName = 'dcu_test_campaign';
 const creativeName = 'dcu_test_300x250';
 const creativeOptions = {
@@ -21,70 +22,296 @@ const creativeOptions = {
     }
 };
 
+// Query Selectors
+
+// for commencing searches
+let qsSearchInput = "#gwt-debug-table-search-input";
+let qsSearchBtn = "#gwt-debug-table-search-button";
+// let qsSearchResult = "td:first-child a[title='" + advertiserName + "' i]";
+
+// for search results page
+let qsLoadingDone = "div.gwt-ScrollTable div#gwt-debug-modal-spinner div[aria-hidden='true']";
+let qsHeaders = 'table.headerTable tr';
+let qsData = 'table.dataTable tr';
+
+const testBrowser = async () => {
+    const testing = false;
+    const testHTML = "html/index.html";
+    userDir = path.resolve(config.get("common.userDataDir"));
+
+    console.log(userDir);
+
+    const browser = await puppeteer.launch({userDataDir: userDir});
+    const page = await browser.newPage();
+
+    console.log("going to google");
+    await page.goto('https://www.google.com');
+    console.log("google loaded")
+
+    // other actions...
+    await browser.close();
+}
+
 (async () => {
-    // const browser = await puppeteer.launch();
     const browser = await puppeteer.launch({
         headless: false,
+        args: ["--start-maximized"],
         defaultViewport: {
             width: 1920,
             height: 1080,
         },
-        userDataDir: './user_data',
-        args: [
-            "--start-maximized"
-        ]
+        userDataDir: path.resolve(config.get("common.userDataDir")),
     });
+
+    const page = await browser.newPage();
+    // const navigationPromise = page.waitForNavigation({
+    //     waitUntil: 'load',
+    // });
+
+    const advertiserExists = await checkIfAdvertiserExists(browser, page, advertiserName);
+    console.log("advertiser exists = " + advertiserExists);
+
+    //const advertiserExists = await checkIfAdvertiserExists(browser, page, advertiserName);
+    // await browser.close();
+})();
+
+const checkIfAdvertiserExists = async (browser, page, advertiserName) => {
+    //check if advertiser exists
+    console.log("checking if advertiser exists: " + advertiserName);
+    console.log("going to page: " + advertisersPageUrl)
+    await page.goto(advertisersPageUrl);
+    console.log("page loaded: " + page.url())
+
+    if (page.url().indexOf(mainPageUrl) === -1) { //if the page loaded is not the page we wanted to load
+        console.log('You are not logged in yet => npm run login');
+        return await browser.close();
+    }
+
+    console.log("wait for search input to become available");
+    // await navigationPromise;
+    await page.waitForSelector(qsSearchInput);
+
+    console.log("enter '" + qsSearchInput + "' into search input")
+    await page.click(qsSearchInput)
+    await page.waitFor(500);
+    await page.type(qsSearchInput, advertiserName);
+
+    console.log("click search button")
+    await page.waitFor(500);
+    await page.click(qsSearchBtn);
+
+    console.log("waiting for loader icon to stop spinning")
+    await page.waitFor(500);
+    await page.waitForSelector(qsLoadingDone);
+    console.log("loading icon is gone, search results should be visible");
+
+    const searchResults = await getSearchResults(page);
+
+    if (searchResults.length === 0) {
+        console.log("creating new advertiser")
+        return false;
+    }
+
+    else {
+        console.log("searching through results")
+        const searchResultsFilters = searchResults.filter(result => result.name.toLowerCase() === advertiserName.toLowerCase());
+        console.log(searchResultsFilters.length + " matches found");
+        return true;
+    }
+}
+
+
+const getSearchResults = async (page) => {
+    let searchResults = [];
+
+    // get headers from search results table
+    const headersArr = await page.evaluate(({qsHeaders}) =>
+        Array.from(document.querySelectorAll(qsHeaders),
+            row => Array.from(row.querySelectorAll('td'),
+                cell => cell.innerText)
+        ), {qsHeaders}
+    );
+    headersArr.shift(); // first row is empty, so removing first entry in array
+    const [headers] = headersArr; // destructure array
+
+    // get results from search results table
+    const dataArr = await page.evaluate(({qsData}) =>
+        Array.from(document.querySelectorAll(qsData),
+            row => Array.from(row.querySelectorAll('td'),
+                cell => cell.innerText)
+        ), {qsData}
+    );
+    dataArr.shift(); // first row is empty, so removing first entry in array
+
+    //turn headers + data arrays into object with keys
+    dataArr.forEach((row) => {
+        let newObj = {};
+        row.forEach((cell, index) => newObj[headers[index].toLowerCase().replace(' ', '_')] = cell)
+        searchResults.push(newObj);
+    });
+
+    return searchResults;
+}
+
+const startUpload = async () => {
+    console.log("starting browser");
+
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ["--start-maximized"],
+        defaultViewport: {
+            width: 1920,
+            height: 1080,
+        },
+        userDataDir: path.resolve(config.get("common.userDataDir")),
+    });
+
     const page = await browser.newPage();
     const navigationPromise = page.waitForNavigation({
         waitUntil: 'load',
     });
 
+    // if (testing) {
+    // await page.goto('file:///C:/Projects/m/mediamonks/puppeteer-test/src/html/index.html');
+    //
+    // let searchResults = [];
+    // // get headers from search results table
+    // const headersArr = await page.evaluate(({qsHeaders}) =>
+    //     Array.from(document.querySelectorAll(qsHeaders),
+    //             row => Array.from(row.querySelectorAll('td'),
+    //                     cell => cell.innerText)
+    //     ), {qsHeaders}
+    // );
+    // headersArr.shift(); // first row is empty, so removing first entry in array
+    // const [headers] = headersArr; // destructure array
+    //
+    // // get results from search results table
+    // const dataArr = await page.evaluate(({qsData}) =>
+    //     Array.from(document.querySelectorAll(qsData),
+    //         row => Array.from(row.querySelectorAll('td'),
+    //             cell => cell.innerText)
+    //     ), {qsData}
+    // );
+    // dataArr.shift(); // first row is empty, so removing first entry in array
+    //
+    // //turn headers + data arrays into object with keys
+    // dataArr.forEach((row) => {
+    //     let newObj = {};
+    //     row.forEach((cell, index) => newObj[headers[index].toLowerCase().replace(' ', '_')] = cell)
+    //     searchResults.push(newObj);
+    // });
+    //
+    //
+    // console.log(searchResults.length + " results. Searching");
+    // const searchResultsFilters = searchResults.filter(result => result.name.toLowerCase() === advertiserName.toLowerCase());
+    // console.log(searchResultsFilters.length + " matches found, advertiser exists");
+    //
+    //
+    // await page.waitFor(5000000);
+    // }
+
 
     // Continue to DC Studio
-    await page.goto('https://www.google.com/doubleclick/studio/');
-    await navigationPromise;
-    await page.screenshot({
-        path: config.get('common.screensFolder') + '/Logged into DC.png',
-    });
-
+    // await page.goto('https://www.google.com/doubleclick/studio/');
+    // await navigationPromise;
+    // await page.screenshot({
+    //     path: config.get('common.screensFolder') + '/Logged into DC.png',
+    // });
 
     //check if advertiser exists
+    console.log("going to page: " + advertisersPageUrl)
     await page.goto(advertisersPageUrl);
+    console.log("page loaded: " + page.url())
+
+    if (page.url().indexOf(mainPageUrl) === -1) { //if the page loaded is not the page we wanted to load
+        console.log('You are not logged in yet => npm run login');
+        return await browser.close();
+    }
+
+    console.log("wait for search input to become available");
     await navigationPromise;
-    console.log("showing Advertisers");
+    await page.waitForSelector(qsSearchInput);
 
-    const searchInput = "#gwt-debug-table-search-input";
-    const searchBtn = "#gwt-debug-table-search-button";
-    const searchResult = "a[title='" + advertiserName + "']"
 
-    await page.waitForSelector(searchInput);
-    console.log("advertiser search input available now.")
-
-    await page.click(searchInput)
+    console.log("enter '" + qsSearchInput + "' into search input")
+    await page.click(qsSearchInput)
     await page.waitFor(500);
-    await page.type(searchInput, advertiserName);
+    await page.type(qsSearchInput, advertiserName);
 
+    console.log("click search button")
     await page.waitFor(500);
-    await page.click(searchBtn);
-
-    console.log("searching for " + searchResult)
-    await page.waitForSelector(searchResult);
-    //await page.waitForSelector("#gwt-debug-creatives-table-name-link-36525044");
+    await page.click(qsSearchBtn);
 
 
-    console.log("found it")
+    //console.log("searching for " + qsSearchResult)
+    await page.waitFor(500);
+    await page.waitForSelector(qsLoadingDone);
+    console.log("loading icon is gone, search results should be visible");
+
+    let searchResults = [];
+    // get headers from search results table
+    const headersArr = await page.evaluate(({qsHeaders}) =>
+        Array.from(document.querySelectorAll(qsHeaders),
+            row => Array.from(row.querySelectorAll('td'),
+                cell => cell.innerText)
+        ), {qsHeaders}
+    );
+    headersArr.shift(); // first row is empty, so removing first entry in array
+    const [headers] = headersArr; // destructure array
+
+    // get results from search results table
+    const dataArr = await page.evaluate(({qsData}) =>
+        Array.from(document.querySelectorAll(qsData),
+            row => Array.from(row.querySelectorAll('td'),
+                cell => cell.innerText)
+        ), {qsData}
+    );
+    dataArr.shift(); // first row is empty, so removing first entry in array
+
+    //turn headers + data arrays into object with keys
+    dataArr.forEach((row) => {
+        let newObj = {};
+        row.forEach((cell, index) => newObj[headers[index].toLowerCase().replace(' ', '_')] = cell)
+        searchResults.push(newObj);
+    });
+
+    console.log(searchResults.length + " results");
+
+    if (searchResults.length === 0) {
+        console.log("creating new advertiser")
+    }
+
+    else {
+        console.log("searching through results")
+        const searchResultsFilters = searchResults.filter(result => result.name.toLowerCase() === advertiserName.toLowerCase());
+        console.log(searchResultsFilters.length + " matches found");
+    }
 
 
+    //await page.focus(searchResult);
+    //const el = await page.$(searchResult)
+    //console.log(el);
+    //
+    // const bodyHandle = await page.$('body');
+    // const html = await page.evaluate(body => body.innerHTML, bodyHandle);
 
 
+    // let name = 'jack';
+    // let age  = 33;
+    // let location = 'Berlin/Germany';
 
+    // await page.evaluate(({advertiserName}) => {
+    //
+    //     //var
+    //
+    //
+    //     // console.log(name);
+    //     // console.log(age);
+    //     // console.log(location);
+    //
+    // },{advertiserName});
 
-
-
-
-
-
-
+    //await bodyHandle.dispose();
 
 
     // //check if creative exists
@@ -98,14 +325,5 @@ const creativeOptions = {
     // console.log("make new creative screen")
 
 
-
-
-
-
-
-
-
     //await browser.close();
-
-
-})();
+}
---
+++ src/upload_file.js
@@ -0,0 +1,162 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+const config = require('config');
+const axios = require('axios');
+const fs = require('fs');
+
+// ONLY USE WHEN DEVELOPING IN CHARLES
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+const getCookie = (cookies, cookieID) => cookies.filter( (cookie) => cookie.name === cookieID);
+
+//const mainPageUrl = 'https://www.google.com/doubleclick/studio/'
+const mainPageUrl = 'https://localhost:65000/doubleclick/studio/';
+const uploadScriptUrl = 'upload/rupio';
+
+const zipFilename = './files/dcu_test_300x250.zip';
+const zipData = fs.createReadStream(zipFilename);
+const zipFilesize = fs.statSync(zipFilename).size;
+
+const accountId = '33345';
+const advertiserId = '36525044';
+const creativeId = '36529182';
+const ownerId = '291343';
+const entityId = '106215518';
+const creativeFormat = 'INPAGE';
+
+const uploadScriptData = {
+    'protocolVersion': '0.8',
+    'createSessionRequest': {
+        'fields': [
+            {
+                'external': {
+                    'name': 'file',
+                    'filename': 'dcu_test_300x250.zip',
+                    'put': {
+
+                    },
+                    'size': zipFilesize
+                }
+            },
+            {
+                'inlined': {
+                    'name': 'TYPE',
+                    'content': 'CREATIVE',
+                    'contentType': 'text/plain'
+                }
+            },
+            {
+                'inlined': {
+                    'name': 'ACCOUNT_ID',
+                    'content': accountId,
+                    'contentType': 'text/plain'
+                }
+            },
+            {
+                'inlined': {
+                    'name': 'ADVERTISER_ID',
+                    'content': advertiserId,
+                    'contentType': 'text/plain'
+                }
+            },
+            {
+                'inlined': {
+                    'name': 'ADVERTISER_OWNER_ID',
+                    'content': ownerId,
+                    'contentType': 'text/plain'
+                }
+            },
+            {
+                'inlined': {
+                    'name': 'CREATIVE_ID',
+                    'content': creativeId,
+                    'contentType': 'text/plain'
+                }
+            },
+            {
+                'inlined': {
+                    'name': 'CREATIVE_ENTITY_ID',
+                    'content': entityId,
+                    'contentType': 'text/plain'
+                }
+            },
+            {
+                'inlined': {
+                    'name': 'CREATIVE_OWNER_ID',
+                    'content': ownerId,
+                    'contentType': 'text/plain'
+                }
+            },
+            {
+                'inlined': {
+                    'name': 'CREATIVE_FORMAT',
+                    'content': creativeFormat,
+                    'contentType': 'text/plain'
+                }
+            }
+        ]
+    }
+};
+
+(async () => {
+
+    const allowed = true;
+
+    // let cookie = {
+    //     name: 'SID',
+    //     value: 'uQftjEb312xRIC_6SimwZAKcOUcZAx8iaFBkqeFsUUwbJqGen9ASO58AvZ5a8iQClG4BqA.'
+    // };
+
+    if (allowed) {
+        const browser = await puppeteer.launch({
+            headless: false,
+            args: ['--start-maximized'],
+            defaultViewport: {
+                width: 1920,
+                height: 1080,
+            },
+            userDataDir: path.resolve(config.get('common.userDataDir')),
+        });
+
+        const page = await browser.newPage();
+        const navigationPromise = page.waitForNavigation({
+            waitUntil: 'load',
+        });
+
+        await page.goto('https://www.google.com/doubleclick/studio/#creative:step=MANAGE_FILES&advertiserId=36525044&creativeId=36529182&ownerId=291343&entityId=106215518');
+        await navigationPromise;
+        const cookies = await page.cookies();
+        const [sidCookie] = getCookie(cookies, 'SID');
+        cookie = sidCookie;
+
+    }
+
+    console.log('Got cookie: '  + cookie.name + '=' + cookie.value)
+
+    // first part, request upload_id
+    const reqUploadIDUrl = mainPageUrl + uploadScriptUrl;
+    const data = uploadScriptData;
+    const params = {
+        'headers': {
+            Cookie: cookie.name + '=' + cookie.value,
+        }
+    };
+    const rupioResultUploadId = await axios.post(reqUploadIDUrl, data, params);
+    console.log(rupioResultUploadId.data)
+
+    const uploadId = rupioResultUploadId.data.sessionStatus.upload_id;
+    console.log('received upload id: ' + uploadId);
+
+    // now second part, upload file
+    const reqUploadFileUrl = mainPageUrl + uploadScriptUrl + '?upload_id=' + uploadId + '&file_id=000';
+    const headers = {
+        Cookie: cookie.name + '=' + cookie.value,
+        'content-type': 'application/octet-stream',
+        'Content-Length': zipFilesize,
+    };
+
+    const rupioResultPostFile = await axios.post(reqUploadFileUrl, zipData, {headers});
+    console.log(rupioResultPostFile.data);
+})();
+
+
