bee03b0e5bfdff212d2daab653947f74a4628e28
Mirko <mirko@mediamonks.com>
2020-10-27 16:15:44 +0100

    Improved logging and some error catching

--- src/doubleclick.js
+++ src/doubleclick.js
@@ -2,108 +2,46 @@ const axios = require('axios');
 const { getUrlParams, getJSON } = require('./utils.js');
 const log = require('./utils.js').log();
 
-
 const waitMs = 1000; // TODO:  Optimize speeds. Figure out where we don't need 1000ms wait time because it adds up
-
-const searchForEntity = async (page, searchQuery) => {
-    // CSS selectors :
-    // for commencing searches
-    const qsSearchInput = '#gwt-debug-table-search-input';
-    const qsSearchBtn = '#gwt-debug-table-search-button';
-    const qsSearchResult = page.url().indexOf('campaignId') !== -1 ? 'td a[title="' + searchQuery + '" i]' : 'td:first-child a[title="' + searchQuery + '" i]';
-
-    // for search results page
-    const qsLoadingDone = "div.gwt-ScrollTable div#gwt-debug-modal-spinner div[aria-hidden='true']";
-    const qsHeaders = 'table.headerTable tr';
-    const qsData = 'table.dataTable tr';
-
-    const searchResults = [];
-
-    log.debug('searching selector: ' + qsSearchInput);
-    await page.waitForSelector(qsSearchInput);
-
-    const isDisabled = await page.$eval(qsSearchInput, (element) => element.hasAttribute('disabled')); // when there are no entities in a advertiser/campaign, the search bar is disabled
-    if (isDisabled) {
-        log.debug('Search is disabled, so there are no search results.');
-        return { exists: false };
-    }
-
-    log.debug('clicking selector: ' + qsSearchInput);
-    await page.click(qsSearchInput);
-    await page.waitFor(waitMs);
-
-    log.debug('filling selector: ' + qsSearchInput);
-    await page.type(qsSearchInput, searchQuery);
-    await page.waitFor(waitMs);
-
-    log.debug('clicking selector: ' + qsSearchBtn);
-    await page.click(qsSearchBtn);
-    await page.waitFor(waitMs);
-
-    log.debug('clicking selector: ' + qsLoadingDone);
-    await page.waitForSelector(qsLoadingDone);
-
-    // get headers from search results table
-    const headersArr = await page.evaluate(({ qsHeaders }) => Array.from(document.querySelectorAll(qsHeaders),
-        (row) => Array.from(row.querySelectorAll('td'),
-            (cell) => cell.innerText)), { qsHeaders });
-    headersArr.shift(); // first row is empty, so removing first entry in array
-    const [headers] = headersArr; // destructure array
-
-    // get results from search results table
-    const dataArr = await page.evaluate(({ qsData }) => Array.from(document.querySelectorAll(qsData),
-        (row) => Array.from(row.querySelectorAll('td'),
-            (cell) => cell.innerText)), { qsData });
-    dataArr.shift(); // first row is empty, so removing first entry in array
-
-    // turn headers + data arrays into object with keys
-    dataArr.forEach((row) => {
-        const newObj = {};
-        row.forEach((cell, index) => newObj[headers[index].toLowerCase().replace(' ', '_')] = cell);
-        searchResults.push(newObj);
-    });
-
-    log.debug('search results parsed, searching through results');
-    const searchResultsFilters = searchResults.filter((result) => result.name.toLowerCase() === searchQuery.toLowerCase());
-    log.info(searchResultsFilters.length + ' matches found');
-
-    if (searchResults.length === 0) {
-        log.debug('no results');
-        return { exists: false };
-    }
-
-    log.debug('searching selector: ' + qsSearchResult);
-    await page.waitForSelector(qsSearchResult);
-
-    log.debug('clicking selector: ' + qsSearchResult);
-    await page.click(qsSearchResult);
-    await page.waitFor(waitMs);
-
-    const urlParams = await getUrlParams(page.url());
-    return { exists: true, url: page.url(), urlParams };
-};
-
 
 exports.getAdvertiser = async (page, uploadConfig, advertiserName) => {
     const advertisersPageUrl = uploadConfig.doubleclick.url + '#advertisers:';
-    // check if advertiser exists
-    log.info('checking if advertiser exists: ' + advertiserName);
     log.debug('going to page: ' + advertisersPageUrl);
     await page.goto(advertisersPageUrl);
     log.debug('page loaded: ' + page.url());
-    return searchForEntity(page, advertiserName);
+    //return searchForEntity(page, advertiserName);
+    const searchResults = await searchForEntity(page, advertiserName)
+    return searchResults;
+
+};
+
+exports.getCampaign = async (page, advertiser, campaignName) => {
+    //log.info('checking if campaign exists: ' + campaignName);
+    log.debug('going to page: ' + advertiser.url);
+    await page.goto(advertiser.url);
+    log.debug('page loaded: ' + page.url());
+    return searchForEntity(page, campaignName);
+};
+
+exports.getCreative = async (page, campaign, creativeName) => {
+    //log.info('checking if creative exists: ' + creativeName);
+    log.debug('going to page: ' + campaign.url);
+    await page.goto(campaign.url);
+    log.debug('page loaded: ' + page.url());
+    await page.waitFor(3000);
+    return searchForEntity(page, creativeName);
 };
 
 exports.createAdvertiser = async (browser, page, uploadConfig, advertiserName) => {
     const createAdvertiserUrl = uploadConfig.doubleclick.url + '#advertiser/new:accountId=' + uploadConfig.doubleclick.accountId + '&accountName=' + uploadConfig.doubleclick.accountName;
+    //const advertiserUrl = 'https://www.google.com/doubleclick/studio/#Advertiser:';
+    const advertiserUrl = uploadConfig.doubleclick.url + '#Advertiser:';
 
     const qsAdvertiserInput = 'input#gwt-debug-advertiser-advertiserName-input';
     const qsAdvertiserSubmit = 'a#gwt-debug-save-button';
     const qsAdvertiserLabel = '#gwt-debug-advertiser-pageTitle-label';
-    const advertiserUrl = 'https://www.google.com/doubleclick/studio/#Advertiser:';
 
-    // check if advertiser exists
-    log.info('creating new advertiser: ' + advertiserName);
+    //log.info('creating new advertiser: ' + advertiserName);
     log.debug('going to page: ' + createAdvertiserUrl);
     await page.goto(createAdvertiserUrl);
     log.debug('page loaded: ' + page.url());
@@ -135,37 +73,16 @@ exports.createAdvertiser = async (browser, page, uploadConfig, advertiserName) =
     return { exists: true, url: page.url(), urlParams };
 };
 
-const waitForUrl = async (page, url) => {
-    const promise = new Promise((resolve) => {
-        const checkUrl = setInterval(() => {
-            log.debug('checking: ' + page.url());
-            if (page.url().indexOf(url) === 0) {
-                clearInterval(checkUrl);
-                resolve(page.url());
-            }
-        }, 200);
-    });
-
-    return promise;
-};
-
-exports.getCampaign = async (page, advertiser, campaignName) => {
-    log.info('checking if campaign exists: ' + campaignName);
-    log.debug('going to page: ' + advertiser.url);
-    await page.goto(advertiser.url);
-    log.debug('page loaded: ' + page.url());
-    return searchForEntity(page, campaignName);
-};
-
 exports.createCampaign = async (browser, page, uploadConfig, advertiser, campaignName) => {
     const createCampaignUrl = uploadConfig. doubleclick.url + '#campaign/new:advertiserId=' + advertiser.urlParams.advertiserId;
+    //const campaignUrl = 'https://www.google.com/doubleclick/studio/#campaign:';
+    const campaignUrl = uploadConfig. doubleclick.url + '#campaign:';
+
     const qsCampaignInput = 'input#gwt-debug-new-campaign-campaignText';
     const qsCampaignSubmit = 'a#gwt-debug-save-button';
     const qsCampaignLabel = 'gwt-debug-campaign-pageTitle-label';
-    const campaignUrl = 'https://www.google.com/doubleclick/studio/#campaign:';
 
-    // check if advertiser exists
-    log.info('creating new campaign: ' + campaignName);
+    //log.info('creating new campaign: ' + campaignName);
     log.debug('going to page: ' + createCampaignUrl);
     await page.goto(createCampaignUrl);
     log.debug('page loaded: ' + page.url());
@@ -195,17 +112,11 @@ exports.createCampaign = async (browser, page, uploadConfig, advertiser, campaig
     return { exists: true, url: page.url(), urlParams };
 };
 
-exports.getCreative = async (page, campaign, creativeName) => {
-    log.info('checking if creative exists: ' + creativeName);
-    log.debug('going to page: ' + campaign.url);
-    await page.goto(campaign.url);
-    log.debug('page loaded: ' + page.url());
-    await page.waitFor(3000);
-    return searchForEntity(page, creativeName);
-};
-
 exports.createCreative = async (browser, page, uploadConfig, advertiser, campaign, creative) => {
     const createCreativeUrl = uploadConfig.doubleclick.url + '#creative/new:campaignId=' + campaign.urlParams.campaignId + '&advertiserId=' + advertiser.urlParams.advertiserId;
+    //const creativeUrl = 'https://www.google.com/doubleclick/studio/#creative:step=MANAGE_FILES';
+    const creativeUrl = uploadConfig.doubleclick.url + '#creative:step=MANAGE_FILES';
+
     const qsCreativeNameInput = 'input#gwt-debug-creativeDetail-nameText';
     const qsCreativeFormatDropdown = 'div#gwt-debug-creativeDetail-formatText';
     const qsCreativeFormatValue = '#gwt-debug-creativeDetail-formatText-' + creative.format;
@@ -217,9 +128,7 @@ exports.createCreative = async (browser, page, uploadConfig, advertiser, campaig
 
     const qsCreativeSubmit = 'a#gwt-debug-creativeworkflow-next-button';
 
-    const creativeUrl = 'https://www.google.com/doubleclick/studio/#creative:step=MANAGE_FILES';
-
-    log.info('creating new creative: ' + creative.name);
+    //log.info('creating new creative: ' + creative.name);
     log.debug('going to page: ' + createCreativeUrl);
     await page.goto(createCreativeUrl);
     log.debug('page loaded: ' + page.url());
@@ -297,8 +206,49 @@ exports.createCreative = async (browser, page, uploadConfig, advertiser, campaig
     await page.waitFor(waitMs);
 
     const urlParams = await getUrlParams(page.url());
-
     return { exists: true, url: page.url(), urlParams };
+};
+
+exports.uploadCreative = async (uploadConfig, uploadJSON, sidCookie, file) => {
+    //const uploadConfig = await getJSON('./upload_config.json');
+    const uploadUrl = uploadConfig.doubleclick.url + 'upload/rupio';
+
+    // first part, request upload_id
+    const data = uploadJSON;
+    const params = {
+        headers: {
+            Cookie: sidCookie.name + '=' + sidCookie.value,
+        },
+    };
+
+    const rupioResultUploadId = await axios.post(uploadUrl, data, params);
+
+    //console.log(rupioResultUploadId.data)
+
+    const uploadId = rupioResultUploadId.data.sessionStatus.upload_id;
+    log.debug('uploading ' + file.name + ', status: ' + rupioResultUploadId.data.sessionStatus.state);
+
+    // now second part, upload file
+    const reqUploadFileUrl = uploadUrl + '?upload_id=' + uploadId + '&file_id=000';
+    const headers = {
+        Cookie: sidCookie.name + '=' + sidCookie.value,
+        'content-type': 'application/octet-stream',
+        'Content-Length': file.size,
+    };
+
+    const rupioResultPostFile = await axios.post(reqUploadFileUrl, file.data, { headers });
+
+    // if (rupioResultPostFile.data.sessionStatus) {
+    //     log.debug('uploading ' + file.name + ', status: ' + rupioResultPostFile.data.sessionStatus.state);
+    // }
+    //
+    // else {
+    //     if (rupioResultPostFile.data.errorMessage) {
+    //
+    //     }
+    // }
+
+    return rupioResultPostFile.data;
 };
 
 exports.composeUploadJSON = (accountId, advertiser, campaign, creative, file) => ({
@@ -375,31 +325,94 @@ exports.composeUploadJSON = (accountId, advertiser, campaign, creative, file) =>
     },
 });
 
-exports.uploadCreative = async (uploadConfig, uploadJSON, sidCookie, file) => {
-    //const uploadConfig = await getJSON('./upload_config.json');
-    const uploadUrl = uploadConfig.doubleclick.url + 'upload/rupio';
+const searchForEntity = async (page, searchQuery) => {
+    // CSS selectors :
+    // for commencing searches
+    const qsSearchInput = '#gwt-debug-table-search-input';
+    const qsSearchBtn = '#gwt-debug-table-search-button';
+    const qsSearchResult = page.url().indexOf('campaignId') !== -1 ? 'td a[title="' + searchQuery + '" i]' : 'td:first-child a[title="' + searchQuery + '" i]';
 
-    // first part, request upload_id
-    const data = uploadJSON;
-    const params = {
-        headers: {
-            Cookie: sidCookie.name + '=' + sidCookie.value,
-        },
-    };
+    // for search results page
+    const qsLoadingDone = "div.gwt-ScrollTable div#gwt-debug-modal-spinner div[aria-hidden='true']";
+    const qsHeaders = 'table.headerTable tr';
+    const qsData = 'table.dataTable tr';
 
-    const rupioResultUploadId = await axios.post(uploadUrl, data, params);
-    const uploadId = rupioResultUploadId.data.sessionStatus.upload_id;
-    log.info('uploading ' + file.name + ', status: ' + rupioResultUploadId.data.sessionStatus.state);
+    const searchResults = [];
 
-    // now second part, upload file
-    const reqUploadFileUrl = uploadUrl + '?upload_id=' + uploadId + '&file_id=000';
-    const headers = {
-        Cookie: sidCookie.name + '=' + sidCookie.value,
-        'content-type': 'application/octet-stream',
-        'Content-Length': file.size,
-    };
+    log.debug('searching selector: ' + qsSearchInput);
+    await page.waitForSelector(qsSearchInput);
 
-    const rupioResultPostFile = await axios.post(reqUploadFileUrl, file.data, { headers });
-    log.info('uploading ' + file.name + ', status: ' + rupioResultPostFile.data.sessionStatus.state);
-    return rupioResultPostFile.data;
+    const isDisabled = await page.$eval(qsSearchInput, (element) => element.hasAttribute('disabled')); // when there are no entities in a advertiser/campaign, the search bar is disabled
+    if (isDisabled) {
+        log.debug('Search is disabled, so there are no search results.');
+        return { exists: false };
+    }
+
+    log.debug('clicking selector: ' + qsSearchInput);
+    await page.click(qsSearchInput);
+    await page.waitFor(waitMs);
+
+    log.debug('filling selector: ' + qsSearchInput);
+    await page.type(qsSearchInput, searchQuery);
+    await page.waitFor(waitMs);
+
+    log.debug('clicking selector: ' + qsSearchBtn);
+    await page.click(qsSearchBtn);
+    await page.waitFor(waitMs);
+
+    log.debug('clicking selector: ' + qsLoadingDone);
+    await page.waitForSelector(qsLoadingDone);
+
+    // get headers from search results table
+    const headersArr = await page.evaluate(({ qsHeaders }) => Array.from(document.querySelectorAll(qsHeaders),
+        (row) => Array.from(row.querySelectorAll('td'),
+            (cell) => cell.innerText)), { qsHeaders });
+    headersArr.shift(); // first row is empty, so removing first entry in array
+    const [headers] = headersArr; // destructure array
+
+    // get results from search results table
+    const dataArr = await page.evaluate(({ qsData }) => Array.from(document.querySelectorAll(qsData),
+        (row) => Array.from(row.querySelectorAll('td'),
+            (cell) => cell.innerText)), { qsData });
+    dataArr.shift(); // first row is empty, so removing first entry in array
+
+    // turn headers + data arrays into object with keys
+    dataArr.forEach((row) => {
+        const newObj = {};
+        row.forEach((cell, index) => newObj[headers[index].toLowerCase().replace(' ', '_')] = cell);
+        searchResults.push(newObj);
+    });
+
+    log.debug('search results parsed, searching through results');
+    const searchResultsFilters = searchResults.filter((result) => result.name.toLowerCase() === searchQuery.toLowerCase());
+    log.debug(searchResultsFilters.length + ' match found, retrieving IDs from url params...');
+
+    if (searchResults.length === 0) {
+        log.debug('no results');
+        return { exists: false };
+    }
+
+    log.debug('searching selector: ' + qsSearchResult);
+    await page.waitForSelector(qsSearchResult);
+
+    log.debug('clicking selector: ' + qsSearchResult);
+    await page.click(qsSearchResult);
+    await page.waitFor(waitMs);
+
+    const urlParams = await getUrlParams(page.url());
+    return { exists: true, url: page.url(), urlParams };
+};
+
+const waitForUrl = async (page, url) => {
+    const promise = new Promise((resolve) => {
+        const checkUrl = setInterval(() => {
+            log.debug('checking: ' + page.url());
+            if (page.url().indexOf(url) === 0) {
+                clearInterval(checkUrl);
+                resolve(page.url());
+            }
+        }, 200);
+    });
+
+    return promise;
 };
--- src/upload.js
+++ src/upload.js
@@ -20,10 +20,10 @@ const log = require('./utils.js').log();
     await restoreCookies(page, uploadConfig.common.cookiesPath);
     const cookies = await page.cookies(uploadConfig.doubleclick.url);
     const expiredCookies = cookies.filter((cookie) => cookie.expires < Math.floor(new Date() / 1000));
-    log.info(expiredCookies.length === 0 ? 'Cookies checked, all good to go' : 'Some of the required cookies have expired, please log in again with \'npm run login\': ' + expiredCookies);
+    log.info(expiredCookies.length === 0 ? 'cookies checked, all good to go' : 'some of the required cookies have expired, please log in again with \'npm run login\': ' + expiredCookies);
 
     // Validate uploadConfig file. Make sure all ID's are in there
-    log.info("Validating upload config file");
+    log.info("validating upload config file");
     const validateResult = validateUploadConfig(uploadConfig);
 
     /* eslint-disable no-await-in-loop */
@@ -31,21 +31,40 @@ const log = require('./utils.js').log();
 
     if (!validateResult.valid) {
         log.debug(validateResult.errors);
-        log.info("Some ID's are missing, retrieving them now");
+        log.info("some ID's are missing, retrieving them now");
 
         // get ID's (advertiserId, campaignId, creativeId, etc), or create new entities, for all entities and save these to uploadConfig for future reference
         for (let q = 0; q < uploadConfig.campaigns.length; q += 1) {
             const data = uploadConfig.campaigns[q];
 
             // check if advertiser exists
+            log.info('checking if advertiser exists: ' + data.advertiser.name);
             let advertiser = await getAdvertiser(page, uploadConfig, data.advertiser.name);
-            if (!advertiser.exists) advertiser = await createAdvertiser(browser, page, uploadConfig, data.advertiser.name);
-            log.info(advertiser);
+
+            if (advertiser.exists) {
+                log.info("advertiser " + data.advertiser.name + " found with ID: "  + advertiser.urlParams.advertiserId + " & ownerId: " + advertiser.urlParams.ownerId)
+            }
+
+            else {
+                log.info("can't find advertiser " + data.advertiser.name + ", creating new")
+                advertiser = await createAdvertiser(browser, page, uploadConfig, data.advertiser.name);
+                log.info("advertiser " + data.advertiser.name + " created with ID: "  + advertiser.urlParams.advertiserId + " & ownerId: " + advertiser.urlParams.ownerId)
+            }
+
 
             // check if campaign exists
+            log.info('checking if campaign exists: ' + data.campaign.name);
             let campaign = await getCampaign(page, advertiser, data.campaign.name);
-            if (!campaign.exists) campaign = await createCampaign(browser, page, uploadConfig, advertiser, data.campaign.name);
-            log.info(campaign);
+
+            if (campaign.exists) {
+                log.info("campaign " + data.campaign.name + " found with ID: "  + campaign.urlParams.campaignId)
+            }
+
+            else {
+                log.info("can't find campaign " + data.campaign.name + ", creating new")
+                campaign = await createCampaign(browser, page, uploadConfig, advertiser, data.campaign.name);
+                log.info("campaign " + data.campaign.name + " created with ID: "  + campaign.urlParams.campaignId)
+            }
 
             // write new values
             uploadConfig.campaigns[q].advertiser.id = advertiser.urlParams.advertiserId;
@@ -55,9 +74,19 @@ const log = require('./utils.js').log();
 
             // check if creative exists
             for (let i = 0; i < data.creatives.length; i += 1) {
+
+                log.info('checking if creative exists: ' + data.creatives[i].name);
                 let creative = await getCreative(page, campaign, data.creatives[i].name);
-                if (!creative.exists) creative = await createCreative(browser, page, uploadConfig, advertiser, campaign, data.creatives[i]);
-                log.info(creative);
+
+                if (creative.exists) {
+                    log.info("creative " + data.creatives[i].name + " found with ID: "  + creative.urlParams.creativeId + " & entityId: " + creative.urlParams.entityId)
+                }
+
+                else {
+                    log.info("can't find creative " + data.creatives[i].name + ", creating new")
+                    creative = await createCreative(browser, page, uploadConfig, advertiser, campaign, data.creatives[i]);
+                    log.info("creative " + data.creatives[i].name + " created with ID: "  + creative.urlParams.creativeId + " & entityId: " + creative.urlParams.entityId)
+                }
 
                 // write new values
                 uploadConfig.campaigns[q].creatives[i].id = creative.urlParams.creativeId;
@@ -68,16 +97,17 @@ const log = require('./utils.js').log();
     }
 
     else {
-        log.info("Uploadconfig validated");
+        log.info("upload config validated");
     }
 
     // upload all creatives
     for (let q = 0; q < uploadConfig.campaigns.length; q += 1) {
         const batch = uploadConfig.campaigns[q];
-        log.debug('starting upload of ' + batch.advertiser.name + ':' + batch.campaign.name + ' with ' + batch.creatives.length + ' creatives.');
+        log.info('starting upload of ' + batch.advertiser.name + ':' + batch.campaign.name + ' with ' + batch.creatives.length + ' creatives');
 
         // check if creative exists
         for (let i = 0; i < batch.creatives.length; i += 1) {
+
             const file = getFileData(batch.creatives[i].source);
             const [sidCookie] = getCookie(cookies, 'SID');
 
@@ -89,7 +119,22 @@ const log = require('./utils.js').log();
                 file,
             );
 
-            await uploadCreative(uploadConfig, uploadJSON, sidCookie, file);
+            log.info("uploading " + file.name)
+
+            const upload = await uploadCreative(uploadConfig, uploadJSON, sidCookie, file);
+
+            if (upload.sessionStatus) {
+                // all good
+            }
+
+            else {
+                if (upload.errorMessage) {
+                    log.info("error uploading file: " + upload.errorMessage.reason);
+                    log.info("cancelling upload. please check upload_config.json for bad/old data");
+                    return;
+                }
+            }
+
         }
     }
 
